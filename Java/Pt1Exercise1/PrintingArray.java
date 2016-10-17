cc75d04e432539d9cbbbaa9f14db3655d7317235
Nikolaos Oikonomou <ns.oikonomou@gmail.com>
2016-10-18 02:10:10 +0300

    Create PrintingArray.java

---
+++ Java/Pt1Exercise1/PrintingArray.java
@@ -0,0 +1,13 @@
+class PrintingArray{
+	public static void main(String args[]){
+		System.out.println("000000001");
+		System.out.println("000000022");
+		System.out.println("000000333");
+		System.out.println("000004444");
+		System.out.println("000055555");
+		System.out.println("000666666");
+		System.out.println("007777777");
+		System.out.println("088888888");
+		System.out.println("999999999");
+	}
+}
