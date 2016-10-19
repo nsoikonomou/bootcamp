7b175a7bbbb30fff1bfe166cbdbc1ae7fee8d4bb
Nikolaos Oikonomou <ns.oikonomou@gmail.com>
2016-10-20 02:27:06 +0300

    Update and rename test.txt to PronicNumbers.java
    Coding Bootcamp: Java Pt1. Exercise 7

---
+++ Java/Pt1Exercise7/PronicNumbers.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+class PronicNumbers{
+	public static void main(String args[]){
+
+	Scanner scanner =  new Scanner(System.in);
+	System.out.print("Enter number of pronic numbers: ");
+	int numbers = scanner.nextInt();
+
+	for (int i = 1; i <= numbers ;i++){
+		System.out.print(i*(i+1) + " ");
+	}
+	System.out.println();
+	}
+}
