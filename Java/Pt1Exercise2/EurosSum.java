c577e7b5a005e40d0c56dcacc05c618f431711f1
Nikolaos Oikonomou <ns.oikonomou@gmail.com>
2016-11-01 01:46:59 +0200

    Update EurosSum.java

--- Java/Pt1Exercise2/EurosSum.java
+++ Java/Pt1Exercise2/EurosSum.java
@@ -1,27 +1,40 @@
+/*
+ * Exercise 2 from the Python tutorial Part 1 on:
+ * https://codeandwork.github.io/courses/prep/pythonTutorial1.html
+
+ * Exercise is to be written in Java.
+ */
+
 import java.util.Scanner;
 
-public class EurosSum2{
-	public static void main(String args[]){
-	Scanner scanner = new Scanner(System.in);
+public class EurosSum {
 
-	int euros[] = {50, 20, 10, 5, 2, 1};
-	int numNotesCoins[] = new int[6];
-	int total = 0;
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
 
-	for (int i=0; i<euros.length; i++){
-		if (i < 4){
-			System.out.print("Enter number of " + euros[i] + " euro banknotes: ");
-		}
-		else {
-			System.out.print("Enter number of " + euros[i] + " euro coins: ");
-		}
-		numNotesCoins[i] = scanner.nextInt();
-	}
+        // Declare and initialize an array of Euro banknotes and coins.
+        int euros[] = {50, 20, 10, 5, 2, 1};
 
-	for (int j=0; j < numNotesCoins.length; j++){
-		total += numNotesCoins[j] * euros[j];
-	}
+        // Declare and initialize an array of the numbers of Euro banknotes and coins.
+        int numNotesCoins[] = new int[6];
 
-	System.out.println("You have " + total + " euros.");
-	}
+        int total = 0;
+
+        for (int i = 0; i < euros.length; i++) {
+            if (i < 4) {
+                System.out.print("Enter number of " + euros[i] + " euro banknotes: ");
+            } else {
+                System.out.print("Enter number of " + euros[i] + " euro coins: ");
+            }
+            // Read the user input, i.e. number of Euro banknotes and coins.
+            numNotesCoins[i] = scanner.nextInt();
+        }
+
+        // Compute the user's total sum in Euros.
+        for (int j = 0; j < numNotesCoins.length; j++) {
+            total += numNotesCoins[j] * euros[j];
+        }
+
+        System.out.println("You have " + total + " euros.");
+    }
 }
