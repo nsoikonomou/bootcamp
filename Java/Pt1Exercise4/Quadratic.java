751fa76097eb86938d978c221df2643265899f8c
Nikolaos Oikonomou <ns.oikonomou@gmail.com>
2016-11-07 01:34:08 +0200

    Update Quadratic.java

--- Java/Pt1Exercise4/Quadratic.java
+++ Java/Pt1Exercise4/Quadratic.java
@@ -1,28 +1,36 @@
+/*
+ * Exercise 4 from the Python tutorial Part 1 on:
+ * https://codeandwork.github.io/courses/prep/pythonTutorial1.html
+
+ * Exercise must be written in Java.
+ */
 
 import java.util.Scanner;
 
-class Quadratic{
-	public static void main(String args[]){
-	Scanner scanner =  new Scanner(System.in);
+public class Quadratic {
 
-	System.out.print("Enter value for 'a': ");
-	double a = scanner.nextDouble();
+    public static void main(String args[]) {
+        // The variable scanner is used to get the user's input.
+        Scanner scanner = new Scanner(System.in);
 
-	System.out.print("Enter value for 'b': ");
-	double b = scanner.nextDouble();
+        // Read the user's input.
+        System.out.print("Enter value for 'a': ");
+        double a = scanner.nextDouble();
 
-	System.out.print("Enter value for 'c': ");
-	double c = scanner.nextDouble();
+        System.out.print("Enter value for 'b': ");
+        double b = scanner.nextDouble();
 
-	if ((Math.pow(b,2) - (4*a*c)) < 0){
-		System.out.println("No real-valued solutions exist.");
-	}
-	else
-	{
-		double sol1 = (-b + Math.sqrt( Math.pow(b,2) - (4*a*c))) / 2*a;
-		double sol2 = (-b - Math.sqrt( Math.pow(b,2) - (4*a*c))) / 2*a;
-		System.out.println("The solutions are " + sol1 + " and " + sol2);
-	}
+        System.out.print("Enter value for 'c': ");
+        double c = scanner.nextDouble();
 
-	}
+        // Compute the solutions.
+        if ((Math.pow(b, 2) - (4 * a * c)) < 0) {
+            System.out.println("No real-valued solutions exist.");
+        } else {
+            double sol1 = (-b + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / 2 * a;
+            double sol2 = (-b - Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / 2 * a;
+            System.out.println("The solutions are " + sol1 + " and " + sol2 + ".");
+        }
+
+    }
 }
