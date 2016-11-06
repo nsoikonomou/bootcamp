b8723a2ebd9cf2567bbbf2cea532ecc9922a5894
Nikolaos Oikonomou <ns.oikonomou@gmail.com>
2016-11-07 01:34:50 +0200

    Update Harmonic.java

--- Java/Pt1Exercise5/Harmonic.java
+++ Java/Pt1Exercise5/Harmonic.java
@@ -1,16 +1,25 @@
+/*
+ * Exercise 5 from the Python tutorial Part 1 on:
+ * https://codeandwork.github.io/courses/prep/pythonTutorial1.html
 
-class Harmonic{
-	public static void main(String args[]){
-	double sum = 1.0;
-		for (int i=1; i<=5 ;i++){
-			if (i==1){
-				System.out.println((int)sum);
-			}
-			if (i > 1){
-				double step = (double)1/i;
-				sum = sum + step;
-				System.out.println(sum);
-			}
-		}
-	}
+ * Exercise must be written in Java.
+ */
+
+public class Harmonic {
+
+    public static void main(String args[]) {
+        double sum = 1.0;
+        for (int i = 1; i <= 5; i++) {
+            // Print the first term of the harmonic sequence.
+            if (i == 1) {
+                System.out.println((int) sum);
+            }
+            // Compute and print the remaining four terms of the harmonic sequence.
+            if (i > 1) {
+                double step = (double) 1 / i;
+                sum = sum + step;
+                System.out.println(sum);
+            }
+        }
+    }
 }
