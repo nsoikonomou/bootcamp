9b688f82d02d4ed647e2f014e2c06aacb88b69ba
Nikolaos Oikonomou <ns.oikonomou@gmail.com>
2016-11-22 19:18:43 +0200

    Update pronicnumbers.py

--- Python/Pt1Exercise7/pronicnumbers.py
+++ Python/Pt1Exercise7/pronicnumbers.py
@@ -1,3 +1,8 @@
+"""
+ Exercise 7 from the Python tutorial Part 1 on:
+ https://codeandwork.github.io/courses/prep/pythonTutorial1.html
+"""
+
 numbers = int(input("Enter number of pronic numbers:"))
 for n in range(1, numbers+1):
     print(n*(n+1), end=" ")
