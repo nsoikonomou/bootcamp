7eb91cb29d492b07d6a97338e2a36ddae2e5a969
Nikolaos Oikonomou <ns.oikonomou@gmail.com>
2016-11-22 17:17:06 +0200

    Update ValidateTIN.java

--- Java/Pt2Exercise1/ValidateTIN.java
+++ Java/Pt2Exercise1/ValidateTIN.java
@@ -1,30 +1,42 @@
+package part02.exercise01;
+
+/*
+ * Exercise 1 from the Python tutorial Part 2 on:
+ * https://codeandwork.github.io/courses/prep/pythonTutorial2.html
+
+ * Exercise must be written in Java.
+ */
 import java.util.Scanner;
 
-class ValidateTIN {
-	public static void main(String args[]){
-	Scanner scanner = new Scanner(System.in);
-	System.out.print("Enter Tax Identification Number: ");
-	String taxNumber = scanner.nextLine();
+public class ValidateTIN {
 
-	double digits[] = new double[8];
-	double checkDigit = Double.parseDouble(taxNumber.substring(8,9));
-	double sumOfPowers = 0.0;
+    public static void main(String args[]) {
 
-	for (int i=0, j=7; i<=7 && j>=0; i++, j--){
-		digits[i] = Double.parseDouble(taxNumber.substring(j,j+1)) * Math.pow(2,i+1);
-	}
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Tax Identification Number: ");
+        // Read the user's input, i.e. the TIN to be checked.
+        String taxNumber = scanner.nextLine();
 
-	for (int x=0; x<digits.length; x++){
-		sumOfPowers = sumOfPowers + digits[x];
-	}
+        int digits[] = new int[8];
+        // Assign the value of the check digit to the variable checkDigit.
+        int checkDigit = Integer.parseInt(taxNumber.substring(8));
+        int sumOfPowers = 0;
 
-	double result = (sumOfPowers % 11) % 10;
-	if (result == checkDigit){
-		System.out.println("Tax Identification Number valid.");
-	}
-	else{
-		System.out.println("Tax Identification Number not valid.");
-	}
+        // Do all necessary calculations and checks.
+        for (int i = 0, j = 7; i <= 7 && j >= 0; i++, j--) {
+            digits[i] = (int) (Integer.parseInt(taxNumber.substring(j, j + 1)) * Math.pow(2, i + 1));
+        }
 
-	}
+        for (int x = 0; x < digits.length; x++) {
+            sumOfPowers = sumOfPowers + digits[x];
+        }
+
+        int result = (sumOfPowers % 11) % 10;
+        if (result == checkDigit) {
+            System.out.println("Tax Identification Number valid.");
+        } else {
+            System.out.println("Tax Identification Number not valid.");
+        }
+
+    }
 }
