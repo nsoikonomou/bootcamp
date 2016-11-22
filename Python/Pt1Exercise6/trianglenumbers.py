e2f4a31abb7122e35e82e356b7000abeb1edc614
Nikolaos Oikonomou <ns.oikonomou@gmail.com>
2016-11-22 19:18:09 +0200

    Update trianglenumbers.py

--- Python/Pt1Exercise6/trianglenumbers.py
+++ Python/Pt1Exercise6/trianglenumbers.py
@@ -1,3 +1,8 @@
+"""
+ Exercise 6 from the Python tutorial Part 1 on:
+ https://codeandwork.github.io/courses/prep/pythonTutorial1.html
+"""
+
 numbers = int(input("Enter number of triangle numbers:"))
 for n in range(1, numbers+1):
     print(int(n*(n+1)/2), end=" ")
