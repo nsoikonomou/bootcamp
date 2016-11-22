f58af8f0e734f6ef2f465f4ac5cf57c576dc67fb
Nikolaos Oikonomou <ns.oikonomou@gmail.com>
2016-11-22 17:20:19 +0200

    Update PrintIn2Lines.java

--- Java/Pt2Exercise3/PrintIn2Lines.java
+++ Java/Pt2Exercise3/PrintIn2Lines.java
@@ -1,18 +1,31 @@
+/*
+ * Exercise 3 from the Python tutorial Part 2 on:
+ * https://codeandwork.github.io/courses/prep/pythonTutorial2.html
+
+ * Exercise must be written in Java.
+ */
+
 import java.util.Scanner;
 
-class PrintIn2Lines{
-	public static void main(String args[]){
-		Scanner scanner = new Scanner(System.in);
-		System.out.print("Enter 10 digit number: ");
-		String numStr = scanner.nextLine();
+public class PrintIn2Lines {
 
-		for (int i = 0; i < numStr.length(); i = i + 2){
-			System.out.print(numStr.charAt(i) + " ");
-		}
-		System.out.println();
-		for (int i = 1; i < numStr.length(); i = i + 2){
-			System.out.print(" " + numStr.charAt(i));
-		}
-		System.out.println();
-	}
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter 10 digit number: ");
+
+        // Read the user's input, i.e. the 10-digit number.
+        String numStr = scanner.nextLine();
+
+        // Print the first line.
+        for (int i = 0; i < numStr.length(); i = i + 2) {
+            System.out.print(numStr.charAt(i) + " ");
+        }
+        System.out.println();
+
+        // Print the second line.
+        for (int i = 1; i < numStr.length(); i = i + 2) {
+            System.out.print(" " + numStr.charAt(i));
+        }
+        System.out.println();
+    }
 }
